c60b7414dc64087256fa49ead98042272667e66c
anawatj <ajarusiripot@gmail.com>
2023-08-06 10:35:05 +0700

    comment

--- solution.rb
+++ solution.rb
@@ -1,10 +1,14 @@
 class Solution
+
+    #EASY
     def intersectionArrays(a,b)
         a & b
     end
+    # MIDIUM
     def sortNumbers(a)
         a.sort()
     end
+    #HARD
     def findPairs(a,n)
        s=a.clone
        s.uniq! == nil ? a.combination(2).to_a.select{|a,b| a+b==n}:a.permutation(2).to_a.select{|a,b| a+b==n}
