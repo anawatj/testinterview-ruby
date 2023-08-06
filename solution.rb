8e4e385ad2769f4361de7ecacf4519a0352bc931
anawatj <ajarusiripot@gmail.com>
2023-08-06 10:35:23 +0700

    comment

--- solution.rb
+++ solution.rb
@@ -1,14 +1,14 @@
 class Solution
 
-    #EASY
+    #EASY 1
     def intersectionArrays(a,b)
         a & b
     end
-    # MIDIUM
+    # MIDIUM 2
     def sortNumbers(a)
         a.sort()
     end
-    #HARD
+    #HARD 3
     def findPairs(a,n)
        s=a.clone
        s.uniq! == nil ? a.combination(2).to_a.select{|a,b| a+b==n}:a.permutation(2).to_a.select{|a,b| a+b==n}
